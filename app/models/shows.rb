f66cd7f634b56876aadb3cad6c61ac1286a725c3
Eric Clark <eclark0013@gmail.com>
2019-08-23 20:32:01 +0000

    Automatically backed up by Learn

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -10,4 +10,21 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     self.minimum(:rating)
   end
+
+  def self.most_popular_show
+    self.find_by(rating: self.lowest_rating)
+  end
+
+  def self.ratings_sum
+    self.sum(:ratings)
+  end
+
+  def self.popular_shows
+    self.where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+    self.order(:name)
+  end
+
 end
